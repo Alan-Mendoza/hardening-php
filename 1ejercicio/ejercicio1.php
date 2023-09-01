b5a54c3d8a03e5b3513c29518c7231599e69d87f
Alan Mendoza <alanrodrigo.mg@gmail.com>
2023-09-01 10:17:19 -0400

    Tipos de comentarios en php grupales y de una sola linea

--- 1ejercicio/ejercicio1.php
+++ 1ejercicio/ejercicio1.php
@@ -1,4 +1,12 @@
 <?php
+// Esto es un comentario de una linea
+
+/*
+ * Esto es un comentario de varias lineas
+ * muy util para explicar cosas
+ * y comentar codigo
+*/
+
 echo "Hola Meerkatbot"; // imprime texto
 
 print_r("Hola Meerkatbot"); // imprime texto y más información que echo
