fa258732d4efc8f60199a4bc243aa00bebff31e2
Alan Mendoza <alanrodrigo.mg@gmail.com>
2023-09-01 10:13:47 -0400

    Formas de imprimir con php echo print_r print

---
+++ 1ejercicio/ejercicio1.php
@@ -0,0 +1,6 @@
+<?php
+echo "Hola Meerkatbot"; // imprime texto
+
+print_r("Hola Meerkatbot"); // imprime texto y más información que echo
+
+print("Hola Meerkatbot"); // imprime tambien información
