75ffb0390f63b05c053aaadd46b1acce6bd4419b
Alan Mendoza <alanrodrigo.mg@gmail.com>
2023-09-04 10:25:43 -0400

    Tipos de datos en PHP

---
+++ 5tiposdedatos/ejercicio5.php
@@ -0,0 +1,7 @@
+<?php
+$edad = 27; // Entero
+$nombre = "Alan"; // String
+$altura = 1.72; // Float o decimal
+$programador = null; // Valor nulo quiere decir que no soy programador
+
+echo "El nombre es " . $nombre . " su edad es " . $edad . " la altura es " . $altura . " el puesto es " . $programador;
