2620c3803ba6519e4ca11a745f7667a3b4757c22
Alan Mendoza <alanrodrigo.mg@gmail.com>
2023-09-04 10:36:33 -0400

    Tipos de constante con define()

---
+++ 6tiposdeconstantes/ejercicio6.php
@@ -0,0 +1,14 @@
+<?php
+$edad = 27; // Varibles
+echo $edad . "<br />";
+
+$edad = 30; // Varibles cambiar valor de la variable
+echo $edad . "<br />";
+
+// Uso de constantes 
+define("NOMBRE", "ALAN");
+// Imprimiendo Constante
+echo NOMBRE; // No se puede dar un nuevo valor a la constante
+
+// NOMBRE = "Alan";
+// echo NOMBRE;
